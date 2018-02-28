18b57027f1c6b15c7851ebfbd51d585be22cd4e8
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 15:40:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,11 @@ else
 end
 
 def take_a_number(katz_deli, name)
+if katz_deli.length == 0 
 katz_deli.push(name)
 katz_deli.each_with_index {|value, index| puts "Welcome, #{value}. You are number #{index + 1} in line."}
+else 
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli} in line."
+end
 end
