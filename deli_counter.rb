5be6c57e2bd5c48c1891482a95549dc8c4cd721b
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 15:28:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+if katz_deli.length == 0
+  puts "The line is currently empty."
+else
+  new_arr = []
+  katz_deli.each_with_index {|value, index| new_arr.push("#{index + 1}. #{value}")}
+  puts "The line is currently: #{new_arr.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+katz_deli.push(name)
+katz_deli.each_with_index {|value, index| puts "Welcome, #{value}. You are number #{index + 1} in line."
+end  
